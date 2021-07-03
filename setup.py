0eaa6368ec2524167e9615831a363df4952fbcdd
VivekBits2210 <viveknayak2210@gmail.com>
2021-07-03 22:10:37 +0530

    Adding setup file

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+	name='pykindler',
+	version='0.1',
+	description="Schedules conversion of e-books to mobi, mails it to your kindle",
+	packages=setuptools.find_packages()
+)

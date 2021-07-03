4f9f256bcc8ca7bf26294ab13018a890cdd309ca
VivekBits2210 <viveknayak2210@gmail.com>
2021-07-03 23:36:19 +0530

    Constants as standalone variables, not dicts

--- pykindler/utils.py
+++ pykindler/utils.py
@@ -1,4 +1,5 @@
 from os import path, makedirs
+from .constants import converted_dir
 # Finds your downloads location
 def get_downloads_folder_location():
     try: #GTK2
@@ -25,14 +26,13 @@ def is_token_good(token):
         return False
     return True
 
-#EMCODE: clean_name = re.sub(r'\b\w{1,2}\b', '', clean_name) #Remove small tokens
 # Clean file names to help find metadata better
 def clean_file_name(filename):
     import re
     extension = filename[filename.rfind('.')+1:]
-    clean_name = filename[:filename.rfind('.')] #Extension removal
-    clean_name = re.sub(r'[^A-Za-z\' ]+', ' ',clean_name) #Keep only letters and spaces
-    clean_name = re.sub(r' +', ' ', clean_name) #Remove extra spaces
+    clean_name = filename[:filename.rfind('.')] 
+    clean_name = re.sub(r'[^A-Za-z\' ]+', ' ',clean_name) 
+    clean_name = re.sub(r' +', ' ', clean_name)
     clean_name = clean_name.strip().lower()
     clean_name = ' '.join([word for word in clean_name.split() if is_token_good(word)])
     return clean_name, extension
@@ -41,3 +41,42 @@ def make_required_directories(dir_list):
     for directory in dir_list:
         if not path.exists(directory):
             makedirs(directory)
+
+def email_book(files, send_to):
+    # TODO: Make this fn work
+    # import smtplib
+    # from pathlib import Path
+    # from email.mime.multipart import MIMEMultipart
+    # from email.mime.base import MIMEBase
+    # from email.mime.text import MIMEText
+    # from email.utils import COMMASPACE, formatdate
+    # from email import encoders
+    
+    
+    # send_from = 'pykindler-noreply@gmail.com'
+    # server = "localhost"
+    # subject = "Sending from pykindler"
+    # message = f"Sending to your kindle ({send_to})"
+    # port = 587
+    # msg = MIMEMultipart()
+
+    # msg['To'] = COMMASPACE.join(send_to)
+    # msg['Date'] = formatdate(localtime=True)
+    # msg['Subject'] = subject
+
+    # msg.attach(MIMEText(message))
+
+    # for file in files:    
+    #     part = MIMEBase('application', "octet-stream")
+    #     with open(path.join(converted_dir), 'rb') as file:
+    #         part.set_payload(file.read())
+    #     encoders.encode_base64(part)
+    #     part.add_header('Content-Disposition',
+    #                     'attachment; filename="{}"'.format(Path(path).name))
+    #     msg.attach(part)
+
+    # smtp = smtplib.SMTP(server, port)
+    # smtp.login(username, password)
+    # msg['From'] = send_from
+    # smtp.sendmail(username, send_to, msg.as_string())
+    # smtp.quit()

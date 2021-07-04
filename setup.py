76fe09843a758cd87a357b478a66c8735cf61a9e
VivekBits2210 <viveknayak2210@gmail.com>
2021-07-04 23:11:37 +0530

    Reformat .py files using Black

--- setup.py
+++ setup.py
@@ -3,22 +3,18 @@ from os import path
 from pykindler import cli
 
 thelibFolder = path.dirname(path.realpath(__file__))
-requirementPath = thelibFolder + '/requirements.txt'
+requirementPath = thelibFolder + "/requirements.txt"
 install_requires = []
 if path.isfile(requirementPath):
     with open(requirementPath) as f:
         install_requires = f.read().splitlines()
 
 setuptools.setup(
-	name='pykindler',
-	install_requires=install_requires,
-	version='0.1',
-	description="Schedules conversion of e-books to mobi, mails it to your kindle",
-	long_description="Schedules conversion of e-books to mobi, mails it to your kindle (repo: https://github.com/VivekBits2210/pykindler)",
-	entry_points={
-        'console_scripts': [
-            'pykindler-run=pykindler.cli:main'
-        ]
-    },
-	packages=setuptools.find_packages()
+    name="pykindler",
+    install_requires=install_requires,
+    version="0.1",
+    description="Schedules conversion of e-books to mobi, mails it to your kindle",
+    long_description="Schedules conversion of e-books to mobi, mails it to your kindle (repo: https://github.com/VivekBits2210/pykindler)",
+    entry_points={"console_scripts": ["pykindler-run=pykindler.cli:main"]},
+    packages=setuptools.find_packages(),
 )

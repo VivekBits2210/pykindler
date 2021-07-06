87fee22aee13fe0006783895b8441d2873425b87
VivekBits2210 <viveknayak2210@gmail.com>
2021-07-06 13:08:43 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ else:
 setuptools.setup(
     name="pykindler",
     install_requires=install_requires,
-    version="0.3.1",
+    version="0.3.2",
     description=description,
     long_description=long_description,
     license="MIT",

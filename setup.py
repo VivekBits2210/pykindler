a35a5a2214bc0bc57b0f33cc82a1db256fa1a217
VivekBits2210 <viveknayak2210@gmail.com>
2021-07-06 12:28:41 +0530

    Bump to 0.2.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ else:
 setuptools.setup(
     name="pykindler",
     install_requires=install_requires,
-    version="0.2",
+    version="0.2.1",
     description=description,
     long_description=long_description,
     license="MIT",
